229a5b954e8814438c24b82fef2f5ab76d5d5bef
unknown <pritombiswas9999@gmail.com>
2025-09-02 00:11:45 +0600

    daily update

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Revisiting-Node
 
 Hi, I am Pritom Biswas and I am revising the features of NodeJS 😊
+
+## Here are the basic overview of the projects till now:
+
+### 1. Hello_World:
+Basics, file systems, imports, exports and some more 😉
+
+### 2. node_server:
+Basic request to the server. Consists both http module and express library oriented approach of sending requests
+
+### 3. project-01:
+Demonstrates the use of GET, POST, PATCH, DELETE headers. Also, demonstrates the use of middlewares (Belive me middlewares are the cores)
--- node_server/index.js
+++ node_server/index.js
@@ -1,35 +1,57 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const express = require('express');
 
-const myServer = http.createServer((pri, tom)=>{ // ideally should be (req, res)
-    // console.dir(pri, {depth: null});
-    // console.log(tom);
+const app = express();
 
-    // console.log(pri.headers);
-    // console.log(pri)
-    const log = `${Date.now()}: ${pri.url}: New request received\n`;
-    const myUrl = url.parse(pri.url, true, true);
-    // console.log(myUrl);
-    // console.log(myUrl.pathname.split('/')[1]);
-    
-    if(pri.url == '/favicon.ico') return tom.end()
-    fs.appendFile('./log.txt', log,  (err, data)=>{
-        if(err) throw err;
-        switch(myUrl.pathname.split('/')[1]){
-            case '': tom.end("It is home page"); break;
-            case 'about': 
-                const username = myUrl.query.my_name;
-                tom.end(`Hi, ${username}`); 
-                break;
-            case 'contact-us': tom.end("It is contact-us page"); break;
-            default: tom.end("Not found"); break;
-        }
-    })
+app.get('/', (req, res)=>{
+    return res.send('Hello from home page');
 });
+
+app.get('/about', (req, res)=>{
+    res.send('hello from about page');
+});
+
+// const myServer = http.createServer((pri, tom)=>{ // ideally should be (req, res)
+//     // console.dir(pri, {depth: null});
+//     // console.log(tom);
+
+//     // console.log(pri.headers);
+//     // console.log(pri)
+//     const log = `${Date.now()}: ${pri.method}: ${pri.url}: New request received\n`;
+//     const myUrl = url.parse(pri.url, true, true);
+//     // console.log(myUrl);
+//     // console.log(myUrl.pathname.split('/')[1]);
+    
+//     if(pri.url == '/favicon.ico') return tom.end()
+//     fs.appendFile('./log.txt', log,  (err, data)=>{
+//         if(err) throw err;
+//         switch(myUrl.pathname.split('/')[1]){
+//             case '': 
+//             if(pri.method == 'GET')
+//                 tom.end("It is home page"); 
+//             break;
+//             case 'about': 
+//                 const username = myUrl.query.my_name;
+//                 tom.end(`Hi, ${username}`); 
+//                 break;
+//             case 'contact-us': 
+//                 tom.end("It is contact-us page"); 
+//                 break;
+//             case 'sign-up':
+//                 if(pri.method === 'GET'){
+//                     tom.end('This is a signup form');
+//                 }else if(pri.method === 'POST'){
+//                     tom.end('success')
+//                 }
+//             default: tom.end("Not found"); break;
+//         }
+//     })
+// });
 
 // console.log(myServer);
 
-myServer.listen(8000, "localhost", 511, ()=>{
+app.listen(8000, "localhost", 511, ()=>{
     console.log("server started");
-})
+});
